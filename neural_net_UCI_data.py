a1527905f5c1c3290ae141b35d4c3dd5e05dad28
jlsotos <jlsotos@cps.edu>
2023-04-25 09:50:37 -0500

    Hopefully a7.py show up this time

--- neural_net_UCI_data.py
+++ neural_net_UCI_data.py
@@ -13,9 +13,9 @@ def parse_line(line: str) -> Tuple[List[float], List[float]]:
     """
     tokens = line.split(",")
     out = int(tokens[0])
-    output = [1 if out == 1 else 0.5 if out == 2 else 1]
+    output = [1 if out == 1 else 0.5 if out == 2 else 1 if out== 3 else 1.5 if out==4 else 2 if out==5 else 2.5 if out==6 else 3]
+    inpt = [float(i) for x in tokens[1:] for i in x i=0 if i=="red" i=1 if i=="blue" i==2 if i=="white" i=3 if i=="orange" i=4 if i=="black" i=5 if i=="gold" i=6 if i=="brown" i=7 if i=="green"]
 
-    inpt = [float(x) for x in tokens[1:]]
     return (inpt, output)
 
 
@@ -45,12 +45,13 @@ def normalize(data: List[Tuple[List[float], List[float]]]):
     return data
 
 
-with open("wine_data.txt", "r") as f:
+with open("flag.txt", "r") as f:
     training_data = [parse_line(line) for line in f.readlines() if len(line) > 4]
-
+for i in range(len(training_data[17][0])):
+    training_data[17][0][i]=[1 if training_data[17][0][i]=="green" else 0 if training_data[17][0][i]=="brown" else 1 if training_data[17][0][i]=="red" else 2 if training_data[17][0][i]=="orange" else 3 if training_data[17][0][i]== "blue" else 4]
 td = normalize(training_data)
 
-nn = NeuralNet(13, 3, 1)
+nn = NeuralNet(29, 3, 1)
 nn.train(td, iters=100_000, print_interval=1000, learning_rate=0.1)
 
 for i in nn.test_with_expected(td):
